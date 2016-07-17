257f0b212f9805d536ba17bdcdbcea6d6af7fbec
Jason Matney <jamatney@gmail.com>
2016-07-17 12:21:46 -0400

    Update L1_BufferAnalysis.py
    Add header + title

--- Lesson1/L1_BufferAnalysis.py
+++ Lesson1/L1_BufferAnalysis.py
@@ -1,1 +1,8 @@
+#--------------#------------------#
+# onGEO@MSU - Python Geoprocessing
+# Lesson 1 - Buffer Analysis
+# -------------#------------------#
+# *** Only copy the line of code below! ***
+# *** Do not copy any of the comments! ***
+
 arcpy.Buffer_analysis("D:\Classes\ONGEO\N005_PY\Course_Materials\Data\SanJuan.gdb\Lakes", "Lakes_buffer.shp", "2 miles", "OUTSIDE_ONLY", "ROUND", "ALL")
