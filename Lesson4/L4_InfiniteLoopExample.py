90ede4c58e5ce46e8761062d991e78aa13130389
Jason Matney <jamatney@gmail.com>
2016-07-17 12:23:56 -0400

    Update L4_InfiniteLoopExample.py
    Add header + title

--- Lesson4/L4_InfiniteLoopExample.py
+++ Lesson4/L4_InfiniteLoopExample.py
@@ -1,4 +1,7 @@
-# InfiniteLoopExample.py
+#--------------#------------------#
+# onGEO@MSU - Python Geoprocessing
+# Lesson 4 - Infinite Loop Example
+# -------------#------------------#
 var = 1
 while var == 1 :  # This constructs an infinite loop
    print "You entered: ", var
