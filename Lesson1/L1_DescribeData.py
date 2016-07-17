f32b5f79b52deee4d28bc28c792327c7257323d2
Jason Matney <jamatney@gmail.com>
2016-07-17 12:22:09 -0400

    Update L1_DescribeData.py
    Add header + title

--- Lesson1/L1_DescribeData.py
+++ Lesson1/L1_DescribeData.py
@@ -1,1 +1,8 @@
+#--------------#------------------#
+# onGEO@MSU - Python Geoprocessing
+# Lesson 1 - Describe Data
+# -------------#------------------#
+# *** Only copy the line of code below! ***
+# *** Do not copy any of the comments! ***
+
 desc = arcpy.Describe("D:\Classes\ONGEO\N005_PY\Course_Materials\Data\SanJuan.gdb\Lakes")
