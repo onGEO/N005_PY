67a6c10965745a4ebaaac9bd782f27a99494490d
Jason Matney <jamatney@gmail.com>
2016-07-16 20:01:16 -0400

    Update L2_ImportArcpy.py
    Deleted unnecessary newline

--- Lesson2/L2_ImportArcpy.py
+++ Lesson2/L2_ImportArcpy.py
@@ -3,7 +3,6 @@
 import arcpy
 arcpy.env.overwriteOutput = True
 
-
 # Set the workspace environment and run Clip_analysis
 arcpy.env.workspace = "D:\Classes\ONGEO\N005_PY\Course_Materials\Data\SanJuan.gdb"
 arcpy.Clip_analysis("Forest", "Wilderness", "Forest_clip", "1.25")
