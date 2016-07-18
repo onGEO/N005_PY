58454735a85011b29614c8c6a9bfa7cc49a7dda7
Jason Matney <jamatney@gmail.com>
2016-07-18 11:02:22 -0400

    Update ClassAndMethod.py

--- Lesson5/ClassAndMethod.py
+++ Lesson5/ClassAndMethod.py
@@ -3,9 +3,9 @@
 # Lesson 5 - Class and Method Example
 # -------------#------------------#
 
-class first_class:
-    def a_method(self):
+class first_class: 
+    def a_method(self): 
         print "You have just used the method, a_method, which is simply a member of the class, first_class "
-
-c = first_class()
-c.a_method() # Prints "You have just used a_method..."
+ 
+c = first_class() 
+c.a_method() # Prints "You have just used the method..."
