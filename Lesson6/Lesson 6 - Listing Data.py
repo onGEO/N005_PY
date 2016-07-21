23f9cff9908c126eb0cd9d33db65ffaf4f5cabf6
Jason Matney <jamatney@gmail.com>
2016-07-21 16:24:46 -0400

    Update Lesson 6 - Listing Data.py

--- Lesson6/Lesson 6 - Listing Data.py
+++ Lesson6/Lesson 6 - Listing Data.py
@@ -3,6 +3,9 @@
 # Lesson 6 - Listing Data
 # -------------#------------------#
 
+# Arguments: 
+# D:\Classes\ONGEO\N005_PY\Course_Materials\Data\SanJuan.gdb\Lakes
+ 
 # import modules
 import arcpy
 
@@ -12,9 +15,7 @@ fc = arcpy.GetParameterAsText(0) # Get user input for a feature class
 fields = arcpy.ListFields(fc)
 desc = arcpy.Describe(fc)
 
-
 print "The name of the feature class is {} \n It has fields: ".format(desc.name)
 
 for field in fields:
-    print "{0}, which is a type of {1} with a length of {2}"
-          .format(field.name, field.type, field.length)
+    print "{0}, which is a type of {1} with a length of {2}".format(field.name, field.type, field.length)
