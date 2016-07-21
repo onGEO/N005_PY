15730ab31fe604d09fb95524998aabc707caae71
Jason Matney <jamatney@gmail.com>
2016-07-21 17:19:58 -0400

    Update and rename Lesson 6 - Listing Data.py to Lesson 6 - Listing Fields.py

--- Lesson6/Lesson 6 - Listing Fields.py
+++ Lesson6/Lesson 6 - Listing Fields.py
@@ -1,13 +1,15 @@
 #--------------#------------------#
 # onGEO@MSU - Python Geoprocessing
-# Lesson 6 - Listing Data
+# Lesson 6 - Listing Fields
 # -------------#------------------#
 
+# ListingFields.py
 # Arguments: 
 # D:\Classes\ONGEO\N005_PY\Course_Materials\Data\SanJuan.gdb\Lakes
  
 # import modules
 import arcpy
+
 
 # For each field in a feature class, print
 #  the field name, type, and length.
@@ -15,7 +17,8 @@ fc = arcpy.GetParameterAsText(0) # Get user input for a feature class
 fields = arcpy.ListFields(fc)
 desc = arcpy.Describe(fc)
 
-print "The name of the feature class is {} \n It has fields: ".format(desc.name)
+
+print "The name of the feature class is {0} \n It has fields: ".format(desc.name)
 
 for field in fields:
     print "{0}, which is a type of {1} with a length of {2}".format(field.name, field.type, field.length)
