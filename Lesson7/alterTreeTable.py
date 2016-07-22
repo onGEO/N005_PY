35eef99daa97896a8ed2f49cbbe2f1a8eeed28db
Jason Andrew Matney <jason@Jason.local>
2016-07-22 13:09:16 -0400

    Add alterTreeTable.py

---
+++ Lesson7/alterTreeTable.py
@@ -0,0 +1,35 @@
+## alterTreeTable.py
+#
+# first import ArcPy and set the workspace
+import arcpy
+arcpy.env.workspace = "D:\Classes\ONGEO\N005_PY\Course_Materials\Data\SanJuan.gdb"
+# Set overwriteOutput to True
+arcpy.env.overwriteOutput = True
+
+# Create a new table named Trees
+arcpy.CreateTable_management("D:\Classes\ONGEO\N005_PY\Course_Materials\Data\SanJuan.gdb", "Trees")
+
+# Add new attribute fields to the new table
+# Add a text field named TREE_NAME
+arcpy.AddField_management("Trees","TREE_NAME", "TEXT")
+# Add a short integer field named COUNT
+arcpy.AddField_management("Trees","COUNT", "SHORT")
+
+# Set a variable named 'rows' equal to a new Insert Cursor using the 'Trees' table
+# This will allow you to add or insert new rows into the table.
+rows = arcpy.InsertCursor("Trees")
+
+# Tell the cursor to create a new row into the table
+row = rows.newRow()
+
+# Set the TREE_NAME and COUNT attributes
+row.TREE_NAME = "Quaking Aspen"
+row.COUNT = 1650
+
+# Insert the row into the table
+rows.insertRow(row)
+
+# Remove the two variables that reference the table
+# This removes the locking on the table and allow it to be used in ArcGIS
+del rows
+del row
