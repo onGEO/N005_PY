334f63d7cac799f8679e2bda04c1a93edc729d86
Jason Andrew Matney <jason@Jason.local>
2016-07-22 15:04:48 -0400

    Include sections to Alter Tree Table

--- Lesson7/alterTreeTable.py
+++ Lesson7/alterTreeTable.py
@@ -3,12 +3,14 @@
 # Lesson 7 - Alter Tree Table
 # -------------#------------------#
 #
-# first import ArcPy and set the workspace
+# Section 1
+# import ArcPy and set the workspace
 import arcpy
 arcpy.env.workspace = "D:\Classes\ONGEO\N005_PY\Course_Materials\Data\SanJuan.gdb"
 # Set overwriteOutput to True
 arcpy.env.overwriteOutput = True
 
+# Section 2
 # Create a new table named Trees
 arcpy.CreateTable_management("D:\Classes\ONGEO\N005_PY\Course_Materials\Data\SanJuan.gdb", "Trees")
 
@@ -18,6 +20,7 @@ arcpy.AddField_management("Trees","TREE_NAME", "TEXT")
 # Add a short integer field named COUNT
 arcpy.AddField_management("Trees","COUNT", "SHORT")
 
+# Section 3
 # Set a variable named 'rows' equal to a new Insert Cursor using the 'Trees' table
 # This will allow you to add or insert new rows into the table.
 rows = arcpy.InsertCursor("Trees")
@@ -32,6 +35,7 @@ row.COUNT = 1650
 # Insert the row into the table
 rows.insertRow(row)
 
+# Section 4
 # Remove the two variables that reference the table
 # This removes the locking on the table and allow it to be used in ArcGIS
 del rows
