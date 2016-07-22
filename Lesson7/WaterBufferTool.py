0b3e03994b7408e01bc225ae47914852c0318234
Jason Andrew Matney <jason@Jason.local>
2016-07-22 13:16:56 -0400

    Add WaterBufferTool.py

---
+++ Lesson7/WaterBufferTool.py
@@ -0,0 +1,12 @@
+import arcpy
+# Create List of feature classes to Buffer
+bufferList = ["Lakes", "Streams"]
+# Initialize a new Python list of feature classes to be Unioned together
+unionList = []
+for fc in bufferList:
+    # Buffer each feature class and dissolve any overlapping polygons
+    arcpy.Buffer_analysis(fc, fc + "Buffer", "1000 meters", "", "ALL")
+    # Add each buffer feature class to a list of feature classes to Union
+    unionList.append(fc + "Buffer")
+# Union the buffered featured classes
+arcpy.Union_analysis(unionList, "WaterBuffers")
